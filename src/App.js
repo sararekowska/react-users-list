8680ca88e2ce31f7ef9f29fe4e5ea7c81e36d753
sararekowska <sara.rekowska@gmail.com>
2021-12-12 12:20:27 +0000

    fetch and show users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+function Users() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setUsers(data);
+      });
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ol>
+      {users.map((user) => (
+        <li>
+          {user.name}
+          <span> @{user.username}</span>
+        </li>
+      ))}
+    </ol>
   );
 }
 
-export default App;
+export default Users;
